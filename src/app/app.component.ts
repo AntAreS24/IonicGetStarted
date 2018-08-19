5dc5723b01b99014c50566f9cb8997ba2ff0ecd6
Ben Amiot <antares24.email@gmail.com>
2018-08-19 22:58:51 +1000

    Added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Firebase } from '@ionic-native/firebase';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,12 +12,22 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private firebase: Firebase) {
     platform.ready().then(() => {
+      this.firebase.grantPermission();
+
+      this.firebase.getToken()
+        .then(token => console.log(`The Firebase token is ${token}`)) // save the token server-side and use it to push notifications to this device
+        .catch(error => console.error('Error getting Firebase token', error));
+
+      this.firebase.onTokenRefresh()
+        .subscribe((token: string) => console.log(`Got a new Firebase token ${token}`));
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+
 }
