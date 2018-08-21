cd8c404954774baaf61109dc08efc02af658b63c
Ben Amiot <antares24.email@gmail.com>
2018-08-21 22:07:36 +1000

    Added AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {Firebase} from '@ionic-native/firebase'
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export config = {
+  apiKey: "",
+  authDomain: "myben-24883.firebaseapp.com",
+  databaseURL: "https://myben-24883.firebaseio.com",
+  projectId: "myben-24883",
+  storageBucket: "myben-24883.appspot.com",
+  messagingSenderId: "411362945334"
+};
+
 @NgModule({
   declarations: [
     MyApp,
